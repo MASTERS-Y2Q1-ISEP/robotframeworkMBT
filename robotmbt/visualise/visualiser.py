3c3d2307275b25f1c039fe6ec4792810e7683690
jonathan <148167.jw@gmail.com>
2025-10-31 11:11:23 +0100

    consistently use "node" in the code

--- robotmbt/visualise/visualiser.py
+++ robotmbt/visualise/visualiser.py
@@ -49,7 +49,7 @@ class NetworkVisualiser:
 
         # graph customisation options
         self.edge_color = "black"
-        self.vertex_radius = 1.0
+        self.node_radius = 1.0
 
     def generate_html(self) -> str:
         """
@@ -72,7 +72,7 @@ class NetworkVisualiser:
 
     def initialise_plot(self):
         """
-        Define plot based on range of coordinates of the vertices.
+        Define plot based on range of coordinates of the nodes.
         Enable tools
         """
         x_range, y_range = zip(*self.graph.pos.values())
@@ -81,9 +81,9 @@ class NetworkVisualiser:
         y_min = min(y_range) - 0.1 * (max(y_range) - min(y_range))
         y_max = max(y_range) + 0.1 * (max(y_range) - min(y_range))
 
-        # scale vertex radius based on range
-        vertices_range = max(x_max-x_min, y_max-y_min)
-        self.vertex_radius = vertices_range / 50
+        # scale node radius based on range
+        nodes_range = max(x_max-x_min, y_max-y_min)
+        self.node_radius = nodes_range / 50
 
         # create plot
         range = Range1d(min(x_min, y_min), max(x_max, y_max))
@@ -98,15 +98,15 @@ class NetworkVisualiser:
 
     def add_labels(self):
         """
-        Add labels to the vertices
+        Add labels to the nodes in bokeh plot
         """
         x_cords = []
         y_cords = []
         labels = []
-        for vertex in self.graph.networkx.nodes:
-            x_cords.append(self.graph.pos[vertex][0])
-            y_cords.append(self.graph.pos[vertex][1]+self.vertex_radius)
-            labels.append(self.graph.networkx.nodes[vertex]['label'])
+        for node in self.graph.networkx.nodes:
+            x_cords.append(self.graph.pos[node][0])
+            y_cords.append(self.graph.pos[node][1]+self.node_radius)
+            labels.append(self.graph.networkx.nodes[node]['label'])
 
         label_source = ColumnDataSource(
             dict(x=x_cords, y=y_cords, label=labels))
@@ -120,7 +120,7 @@ class NetworkVisualiser:
         """
         graph_renderer = from_networkx(self.graph.networkx, self.graph.pos)
         graph_renderer.node_renderer.glyph = Circle(
-            radius=self.vertex_radius, fill_color=Spectral4[0])
+            radius=self.node_radius, fill_color=Spectral4[0])
         graph_renderer.edge_renderer.glyph = MultiLine(
             line_color=self.edge_color, line_alpha=0.7, line_width=2)
         self.plot.renderers.append(graph_renderer)
@@ -131,16 +131,16 @@ class NetworkVisualiser:
         """
         loop = Bezier(
             # starting point
-            x0=x + self.vertex_radius,
+            x0=x + self.node_radius,
             y0=y,
             # end point
             x1=x,
-            y1=y - self.vertex_radius,
+            y1=y - self.node_radius,
             # control points
-            cx0=x + 5*self.vertex_radius,
+            cx0=x + 5*self.node_radius,
             cy0=y,
             cx1=x,
-            cy1=y - 5*self.vertex_radius,
+            cy1=y - 5*self.node_radius,
             # styling
             line_color=self.edge_color,
             line_width=2,
@@ -151,9 +151,9 @@ class NetworkVisualiser:
         # add arrow head
         arrow = Arrow(end=NormalHead(size=10, line_color=self.edge_color, fill_color=self.edge_color),
                       x_start=x,
-                      y_start=y-self.vertex_radius-0.01,
+                      y_start=y-self.node_radius-0.01,
                       x_end=x,
-                      y_end=y-self.vertex_radius)
+                      y_end=y-self.node_radius)
         self.plot.add_layout(arrow)
 
     def add_arrow(self, x0: float, y0: float, x1: float, y1: float):
@@ -164,8 +164,8 @@ class NetworkVisualiser:
         dy = y1 - y0
         length = sqrt(dx**2 + dy**2)
         arrow = Arrow(end=NormalHead(size=10, line_color=self.edge_color, fill_color=self.edge_color),
-                      x_start=x0 + dx / length * self.vertex_radius,
-                      y_start=y0 + dy / length * self.vertex_radius,
-                      x_end=x1 - dx / length * self.vertex_radius,
-                      y_end=y1 - dy / length * self.vertex_radius)
+                      x_start=x0 + dx / length * self.node_radius,
+                      y_start=y0 + dy / length * self.node_radius,
+                      x_end=x1 - dx / length * self.node_radius,
+                      y_end=y1 - dy / length * self.node_radius)
         self.plot.add_layout(arrow)

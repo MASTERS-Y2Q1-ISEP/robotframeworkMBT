87554dc562d297247508f71a4556a6ac3e05dd28
Douwe Osinga <douwe@oscom.nl>
2025-11-04 14:40:44 +0100

    moved width/height/edge styling/... to constants

--- robotmbt/visualise/visualiser.py
+++ robotmbt/visualise/visualiser.py
@@ -18,6 +18,10 @@ class Visualiser:
     a simple interface through which the graph can be updated,
     and retrieved in HTML format.
     """
+    GRAPH_WIDTH_PX      :int = 600 # in px
+    GRAPH_HEIGHT_PX     :int = 400 # in px
+    GRAPH_PADDING_PERC  :int = 15  # %
+    MAX_VERTEX_NAME_LEN :int = 20 # no. of characters
 
     def __init__(self):
         self.graph = ScenarioGraph()
@@ -35,7 +39,7 @@ class Visualiser:
         self.graph.calculate_pos()
         networkvisualiser = NetworkVisualiser(self.graph)
         html_bokeh = networkvisualiser.generate_html()
-        str = f"<iframe srcdoc=\"{html.escape(html_bokeh)}\", width=\"400\", height=\"400\"></iframe>"
+        str = f"<iframe srcdoc=\"{html.escape(html_bokeh)}\", width=\"{Visualiser.GRAPH_WIDTH_PX}px\", height=\"{Visualiser.GRAPH_HEIGHT_PX}px\"></iframe>"
 
         return str
 
@@ -45,12 +49,16 @@ class NetworkVisualiser:
     Generate plot with Bokeh
     """
 
+    EDGE_WIDTH :float = 2.0
+    EDGE_ALPHA :float = 0.7
+    EDGE_COLOUR :str|tuple[int,int,int]  = (12,12,12) # 'visual studio black'
+
+
     def __init__(self, graph: ScenarioGraph):
         self.plot = None
         self.graph = graph
 
         # graph customisation options
-        self.edge_color = "black"
         self.node_radius = 1.0
 
     def generate_html(self) -> str:
@@ -75,11 +83,13 @@ class NetworkVisualiser:
         Define plot based on range of coordinates of the nodes.
         Enable tools
         """
+        padding :float = Visualiser.GRAPH_PADDING_PERC / 100
+
         x_range, y_range = zip(*self.graph.pos.values())
-        x_min = min(x_range) - 0.1 * (max(x_range) - min(x_range))
-        x_max = max(x_range) + 0.1 * (max(x_range) - min(x_range))
-        y_min = min(y_range) - 0.1 * (max(y_range) - min(y_range))
-        y_max = max(y_range) + 0.1 * (max(y_range) - min(y_range))
+        x_min = min(x_range) - padding * (max(x_range) - min(x_range))
+        x_max = max(x_range) + padding * (max(x_range) - min(x_range))
+        y_min = min(y_range) - padding * (max(y_range) - min(y_range))
+        y_max = max(y_range) + padding * (max(y_range) - min(y_range))
 
         # scale node radius based on range
         nodes_range = max(x_max-x_min, y_max-y_min)
@@ -88,7 +98,8 @@ class NetworkVisualiser:
         # create plot
         range = Range1d(min(x_min, y_min), max(x_max, y_max))
 
-        self.plot = Plot(width=400, height=400,
+        self.plot = Plot(width=Visualiser.GRAPH_WIDTH_PX, 
+                         height=Visualiser.GRAPH_HEIGHT_PX,
                          x_range=range,
                          y_range=range)
 
@@ -100,23 +111,32 @@ class NetworkVisualiser:
         """
         Add labels to the nodes in bokeh plot
         """
+
         x_cords = []
         y_cords = []
         labels = []
         for node in self.graph.networkx.nodes:
             x_cords.append(self.graph.pos[node][0])
             y_cords.append(self.graph.pos[node][1]+self.node_radius)
-            labels.append(self.graph.networkx.nodes[node]['label'])
+            labels.append(
+                self._cap_name(self.graph.networkx.nodes[node]['label'])
+            )
+            
             bokeh_node = Circle(radius=self.node_radius,
                                 fill_color=Spectral4[0],
                                 x=self.graph.pos[node][0],
                                 y=self.graph.pos[node][1])
+            
             self.plot.add_glyph(bokeh_node)
 
         label_source = ColumnDataSource(
-            dict(x=x_cords, y=y_cords, label=labels))
+            dict(x=x_cords, y=y_cords, label=labels)
+        )
+
         labels = LabelSet(x="x", y="y", text="label", source=label_source,
-                          text_color="black", text_align="center")
+                          text_color=NetworkVisualiser.EDGE_COLOUR, 
+                          text_align="center")
+        
         self.plot.add_layout(labels)
 
     def add_self_loop(self, x: float, y: float):
@@ -127,27 +147,34 @@ class NetworkVisualiser:
             # starting point
             x0=x + self.node_radius,
             y0=y,
+            
             # end point
             x1=x,
             y1=y - self.node_radius,
+            
             # control points
             cx0=x + 5*self.node_radius,
             cy0=y,
             cx1=x,
             cy1=y - 5*self.node_radius,
+            
             # styling
-            line_color=self.edge_color,
-            line_width=2,
-            line_alpha=0.7
+            line_color=NetworkVisualiser.EDGE_COLOUR,
+            line_width=NetworkVisualiser.EDGE_WIDTH,
+            line_alpha=NetworkVisualiser.EDGE_ALPHA,
         )
         self.plot.add_glyph(loop)
 
         # add arrow head
-        arrow = Arrow(end=NormalHead(size=10, line_color=self.edge_color, fill_color=self.edge_color),
-                      x_start=x,
-                      y_start=y-self.node_radius-0.01,
-                      x_end=x,
-                      y_end=y-self.node_radius)
+        arrow = Arrow(
+            end=NormalHead(size=10, 
+                           line_color=NetworkVisualiser.EDGE_COLOUR, 
+                           fill_color=NetworkVisualiser.EDGE_COLOUR),
+                           
+            x_start=x, y_start=y-self.node_radius-0.01, # -0.01 to guarantee that arrow points upwards. 
+            x_end=x, y_end=y-self.node_radius
+        )
+
         self.plot.add_layout(arrow)
 
     def add_arrow(self, x0: float, y0: float, x1: float, y1: float):
@@ -156,10 +183,26 @@ class NetworkVisualiser:
         """
         dx = x1 - x0
         dy = y1 - y0
-        length = sqrt(dx**2 + dy**2)
-        arrow = Arrow(end=NormalHead(size=10, line_color=self.edge_color, fill_color=self.edge_color),
-                      x_start=x0 + dx / length * self.node_radius,
-                      y_start=y0 + dy / length * self.node_radius,
-                      x_end=x1 - dx / length * self.node_radius,
-                      y_end=y1 - dy / length * self.node_radius)
+        length = sqrt(dx**2 + dy**2) # she dx on my dy till I pythagoras
+        
+        arrow = Arrow(
+            end=NormalHead(
+                size=10, 
+                line_color=NetworkVisualiser.EDGE_COLOUR, 
+                fill_color=NetworkVisualiser.EDGE_COLOUR),
+
+            x_start=x0 + dx / length * self.node_radius,
+            y_start=y0 + dy / length * self.node_radius,
+            x_end=x1 - dx / length * self.node_radius,
+            y_end=y1 - dy / length * self.node_radius
+        )
+
         self.plot.add_layout(arrow)
+
+
+    @staticmethod
+    def _cap_name(name :str) -> str:
+        if len(name) < Visualiser.MAX_VERTEX_NAME_LEN:
+            return name
+    
+        return f"{name[:(Visualiser.MAX_VERTEX_NAME_LEN-3)]}..."

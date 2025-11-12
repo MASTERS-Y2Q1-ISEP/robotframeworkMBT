7297814af63ba1356118dc9e5a8b31eba2d556ed
Thomas <t.s.kas@student.utwente.nl>
2025-11-12 10:57:52 +0100

    ChatGPT's PointDrawTool (for reference)

--- robotmbt/visualise/visualiser.py
+++ robotmbt/visualise/visualiser.py
@@ -4,7 +4,8 @@ from bokeh.models import (
     Plot, Range1d, Circle,
     Arrow, NormalHead, LabelSet,
     Bezier, ColumnDataSource, ResetTool,
-    SaveTool, WheelZoomTool, PanTool
+    SaveTool, WheelZoomTool, PanTool,
+    PointDrawTool, CustomJS, MultiLine, GlyphRenderer,
 )
 from bokeh.embed import file_html
 from bokeh.resources import CDN
@@ -40,167 +41,164 @@ class Visualiser:
 
 
 class NetworkVisualiser:
-    """
-    Generate plot with Bokeh
-    """
-
     EDGE_WIDTH: float = 2.0
     EDGE_ALPHA: float = 0.7
-    EDGE_COLOUR: str | tuple[int, int, int] = (
-        12, 12, 12)  # 'visual studio black'
+    EDGE_COLOUR: str | tuple[int, int, int] = (12, 12, 12)  # Visual Studio black
 
-    # TODO: abstract Graph class, e.g. using abc
     def __init__(self, graph: ScenarioGraph):
         self.plot = None
         self.graph = graph
         self.labels = dict(x=[], y=[], label=[])
-
-        # graph customisation options
+        self.label_source = None
         self.node_radius = 1.0
 
     def generate_html(self) -> str:
-        """
-        Generate html file from networkx graph via Bokeh
-        """
         self._initialise_plot()
-        self._add_edges()
-        self._add_nodes()
-        label_source = ColumnDataSource(data=self.labels)
-        labels = LabelSet(x="x", y="y", text="label", source=label_source,
+        node_renderer, node_source = self._add_nodes()
+        self._add_edges(node_source)
+        draw_tool = PointDrawTool(renderers=[node_renderer], empty_value='black', add=False)
+        self.plot.add_tools(draw_tool)
+        self.plot.toolbar.active_drag = draw_tool
+
+        labels = LabelSet(x="x", y="y", text="label", source=self.label_source,
                           text_color=NetworkVisualiser.EDGE_COLOUR,
                           text_align="center")
-
         self.plot.add_layout(labels)
 
         return file_html(self.plot, CDN, "graph")
 
     def _initialise_plot(self):
-        """
-        Define plot with width, height, x_range, y_range and enable tools.
-        x_range and y_range are padded. Plot needs to be a square
-        """
         padding: float = Visualiser.GRAPH_PADDING_PERC / 100
-
         x_range, y_range = zip(*self.graph.pos.values())
         x_min = min(x_range) - padding * (max(x_range) - min(x_range))
         x_max = max(x_range) + padding * (max(x_range) - min(x_range))
         y_min = min(y_range) - padding * (max(y_range) - min(y_range))
         y_max = max(y_range) + padding * (max(y_range) - min(y_range))
 
-        # scale node radius based on range
-        nodes_range = max(x_max-x_min, y_max-y_min)
+        nodes_range = max(x_max - x_min, y_max - y_min)
         self.node_radius = nodes_range / 50
-
-        # create plot
-        x_range = Range1d(min(x_min, y_min), max(x_max, y_max))
-        y_range = Range1d(min(x_min, y_min), max(x_max, y_max))
 
         self.plot = Plot(width=Visualiser.GRAPH_SIZE_PX,
                          height=Visualiser.GRAPH_SIZE_PX,
-                         x_range=x_range,
-                         y_range=y_range)
-
-        # add tools
-        self.plot.add_tools(ResetTool(), SaveTool(),
-                            WheelZoomTool(), PanTool())
+                         x_range=Range1d(min(x_min, y_min), max(x_max, y_max)),
+                         y_range=Range1d(min(x_min, y_min), max(x_max, y_max)))
+        self.plot.add_tools(ResetTool(), SaveTool(), WheelZoomTool(), PanTool())
 
     def _add_nodes(self):
-        """
-        Add labels to the nodes in bokeh plot
-        """
-        node_labels = nx.get_node_attributes(self.graph.networkx, "label")
-        for node in self.graph.networkx.nodes:
-            # prepare adding labels
-            self.labels['x'].append(self.graph.pos[node][0])
-            self.labels['y'].append(self.graph.pos[node][1]+self.node_radius)
-            self.labels['label'].append(self._cap_name(node_labels[node]))
-
-            # add node
-            bokeh_node = Circle(radius=self.node_radius,
-                                fill_color=Spectral4[0],
-                                x=self.graph.pos[node][0],
-                                y=self.graph.pos[node][1])
-
-            self.plot.add_glyph(bokeh_node)
-
-    def add_self_loop(self, x: float, y: float, label: str):
-        """
-        Self-loop as a Bezier curve with arrow head
-        """
-        loop = Bezier(
-            # starting point
-            x0=x + self.node_radius,
-            y0=y,
-
-            # end point
-            x1=x,
-            y1=y - self.node_radius,
-
-            # control points
-            cx0=x + 5*self.node_radius,
-            cy0=y,
-            cx1=x,
-            cy1=y - 5*self.node_radius,
-
-            # styling
-            line_color=NetworkVisualiser.EDGE_COLOUR,
-            line_width=NetworkVisualiser.EDGE_WIDTH,
-            line_alpha=NetworkVisualiser.EDGE_ALPHA,
+        node_data = dict(
+            id=[n for n in self.graph.networkx.nodes],
+            x=[self.graph.pos[n][0] for n in self.graph.networkx.nodes],
+            y=[self.graph.pos[n][1] for n in self.graph.networkx.nodes],
+            label=[self._cap_name(self.graph.networkx.nodes[n].get("label", "")) for n in self.graph.networkx.nodes],
         )
-        self.plot.add_glyph(loop)
+        node_source = ColumnDataSource(node_data)
 
-        # add arrow head
-        arrow = Arrow(
-            end=NormalHead(size=10,
-                           line_color=NetworkVisualiser.EDGE_COLOUR,
-                           fill_color=NetworkVisualiser.EDGE_COLOUR),
+        node_renderer = self.plot.add_glyph(node_source,
+                                            Circle(radius=self.node_radius, fill_color=Spectral4[0]))
 
-            # -0.01 to guarantee that arrow points upwards.
-            x_start=x, y_start=y-self.node_radius-0.01,
-            x_end=x, y_end=y-self.node_radius
-        )
+        # store labels for node and edge placement
+        self.labels['x'].extend(node_data['x'])
+        self.labels['y'].extend([y + self.node_radius for y in node_data['y']])
+        self.labels['label'].extend(node_data['label'])
 
-        # add edge label
-        self.labels['x'].append(x + self.node_radius)
-        self.labels['y'].append(y - 4*self.node_radius)
-        self.labels['label'].append(label)
+        return node_renderer, node_source
 
-        self.plot.add_layout(arrow)
+    def _add_edges(self, node_source: ColumnDataSource):
+        edges = list(self.graph.networkx.edges())
+        xs, ys, edge_labels = [], [], []
 
-    def _add_edges(self):
-        edge_labels = nx.get_edge_attributes(self.graph.networkx, "label")
-        for edge in self.graph.networkx.edges():
-            x0, y0 = self.graph.pos[edge[0]]
-            x1, y1 = self.graph.pos[edge[1]]
-            if edge[0] == edge[1]:
-                self.add_self_loop(
-                    x=x0, y=y0, label=self._cap_name(edge_labels[edge]))
+        for start, end in edges:
+            x0, y0 = self.graph.pos[start]
+            x1, y1 = self.graph.pos[end]
+            xs.append([x0, x1])
+            ys.append([y0, y1])
+            edge_labels.append(self._cap_name(self.graph.networkx.edges[start, end].get("label", "")))
 
-            else:
-                # edge between 2 different nodes
-                dx = x1 - x0
-                dy = y1 - y0
+        edge_source = ColumnDataSource(data=dict(
+            xs=xs, ys=ys, start=[s for s, _ in edges], end=[t for _, t in edges]
+        ))
 
-                length = sqrt(dx**2 + dy**2)
+        edge_renderer = GlyphRenderer(data_source=edge_source,
+                                      glyph=MultiLine(xs="xs", ys="ys",
+                                                      line_color=NetworkVisualiser.EDGE_COLOUR,
+                                                      line_width=NetworkVisualiser.EDGE_WIDTH,
+                                                      line_alpha=NetworkVisualiser.EDGE_ALPHA))
+        self.plot.renderers.append(edge_renderer)
 
-                arrow = Arrow(
-                    end=NormalHead(
-                        size=10,
-                        line_color=NetworkVisualiser.EDGE_COLOUR,
-                        fill_color=NetworkVisualiser.EDGE_COLOUR),
+        # arrows
+        arrows = []
+        for start, end in edges:
+            arrow = Arrow(
+                end=NormalHead(size=10,
+                               line_color=NetworkVisualiser.EDGE_COLOUR,
+                               fill_color=NetworkVisualiser.EDGE_COLOUR),
+                x_start=self.graph.pos[start][0],
+                y_start=self.graph.pos[start][1],
+                x_end=self.graph.pos[end][0],
+                y_end=self.graph.pos[end][1],
+            )
+            self.plot.add_layout(arrow)
+            arrows.append(arrow)
+        edge_source.data["arrows"] = arrows
 
-                    x_start=x0 + dx / length * self.node_radius,
-                    y_start=y0 + dy / length * self.node_radius,
-                    x_end=x1 - dx / length * self.node_radius,
-                    y_end=y1 - dy / length * self.node_radius
-                )
+        # labels: node + edge labels
+        self.label_source = ColumnDataSource(data=dict(
+            x=self.labels["x"] + [(x0+x1)/2 for x0, x1 in xs],
+            y=self.labels["y"] + [(y0+y1)/2 for y0, y1 in ys],
+            label=self.labels["label"] + edge_labels
+        ))
 
-                self.plot.add_layout(arrow)
+        # JS callback to update everything dynamically
+        update_edges = CustomJS(args=dict(
+            node_source=node_source,
+            edge_source=edge_source,
+            label_source=self.label_source,
+            plot=self.plot
+        ), code="""
+        const node_data = node_source.data;
+        const edge_data = edge_source.data;
+        const label_data = label_source.data;
+        const xmap = {}, ymap = {};
+        for (let i = 0; i < node_data.id.length; i++) {
+            xmap[node_data.id[i]] = node_data.x[i];
+            ymap[node_data.id[i]] = node_data.y[i];
+        }
 
-                # add edge label
-                self.labels['x'].append((x0+x1)/2)
-                self.labels['y'].append((y0+y1)/2)
-                self.labels['label'].append(self._cap_name(edge_labels[edge]))
+        // edges + arrows
+        for (let i = 0; i < edge_data.start.length; i++) {
+            const s = edge_data.start[i];
+            const e = edge_data.end[i];
+            edge_data.xs[i] = [xmap[s], xmap[e]];
+            edge_data.ys[i] = [ymap[s], ymap[e]];
+            const arrow = edge_data.arrows[i];
+            arrow.x_start = xmap[s];
+            arrow.y_start = ymap[s];
+            arrow.x_end = xmap[e];
+            arrow.y_end = ymap[e];
+        }
+
+        // node labels
+        for (let i = 0; i < node_data.id.length; i++) {
+            label_data.x[i] = node_data.x[i];
+            label_data.y[i] = node_data.y[i] + 0.05;
+        }
+
+        // edge labels
+        const offset = node_data.id.length;
+        for (let i = 0; i < edge_data.start.length; i++) {
+            const s = edge_data.start[i];
+            const e = edge_data.end[i];
+            label_data.x[i + offset] = (xmap[s] + xmap[e]) / 2;
+            label_data.y[i + offset] = (ymap[s] + ymap[e]) / 2;
+        }
+
+        edge_source.change.emit();
+        label_source.change.emit();
+        plot.request_render();
+        """)
+
+        node_source.js_on_change("data", update_edges)
+
 
     @staticmethod
     def _cap_name(name: str) -> str:

0d03dc2c42cbbcc7273c23029ff157216163c472
jonathan <148167.jw@gmail.com>
2025-12-19 15:05:16 +0100

    [WIP] generate test suite

--- atest/resources/helpers/modelgenerator.py
+++ atest/resources/helpers/modelgenerator.py
@@ -5,6 +5,44 @@ from robotmbt.visualise.visualiser import Visualiser
 
 
 class ModelGenerator:
+    @keyword(name='Generate Trace Information') # type:ignore
+    def generate_trace_information(self) -> TraceInfo:
+        return TraceInfo()
+
+    @keyword(name='Current Trace Contains') # type:ignore
+    def current_trace_contains(self, trace_info: TraceInfo, scenario_name: str, state_str: str) -> TraceInfo:
+        '''
+        State should be of format
+        "name: key=value"
+        '''
+        scenario = ScenarioInfo(scenario_name)
+        s = state_str.split(': ')
+        key, item = s[1].split('=')
+        state = StateInfo._create_state_with_prop(s[0], [(key, item)])
+        trace_info.update_trace(scenario, state, trace_info.previous_length+1)
+
+        return trace_info
+    
+    @keyword(name='All Traces Contains List') # type:ignore
+    def all_traces_contains_list(self, trace_info: TraceInfo) -> TraceInfo:
+        trace_info.all_traces.append([[]])
+        return trace_info
+    
+    @keyword(name='All Traces Contains') # type:ignore
+    def all_traces_contains(self, trace_info: TraceInfo, scenario_name: str, state_str: str) -> TraceInfo:
+        '''
+        State should be of format
+        "name: key=value"
+        '''
+        scenario = ScenarioInfo(scenario_name)
+        s = state_str.split(': ')
+        key, item = s[1].split('=')
+        state = StateInfo._create_state_with_prop(s[0], [(key, item)])
+        
+        trace_info.all_traces[0].append((scenario, state))
+
+        return trace_info
+
     @keyword(name="Test Suite From JSON")  # type:ignore
     def test_suite_from_json(self, filename: str) -> TraceInfo:
         with open(f'json/{filename}.json', 'r') as f:
@@ -13,7 +51,3 @@ class ModelGenerator:
         visualiser = Visualiser(
             'state', trace_info=decoded_instance)
         return visualiser.trace_info
-
-    @keyword(name="Get Current Trace From TraceInfo")  # type:ignore
-    def get_current_trace_from_traceinfo(self, traceInfo: TraceInfo) -> list[tuple[ScenarioInfo, StateInfo]]:
-        return traceInfo.current_trace
--- atest/robotMBT tests/10_visualisation_tests/import.robot
+++ atest/robotMBT tests/10_visualisation_tests/import.robot
@@ -3,12 +3,15 @@ Resource          ../../resources/visualisation.resource
 Library           robotmbt    processor=echo
 
 *** Test Cases ***
+Create test suite
+    Given test suite s
+    Then test suite s has trace info t
+    and trace info t has current trace c 
+    and current trace c has a tuple 'scenario i, state p: v=1'
+    and current trace c has a tuple 'scenario j, state p: v=2'
+    and trace info t has all traces a
+    and all traces a has list l
+    and list l has a tuple 'scenario i, state p: v=2'
+
 Load json file into robotmbt 
     Given test suite s with flag from_json=atest_resource
-    When test suite s is processed
-    Then test suite s has trace info t
-    Then trace info t has current trace c 
-    Then current trace c has a tuple of scenario a, state p: v=1
-    Then current trace c has a tuple of scenario b, state p: v=2
-    Then trace info t has all traces a
-    Then all traces a has a list with a tuple with scenario a and state p: v=2
